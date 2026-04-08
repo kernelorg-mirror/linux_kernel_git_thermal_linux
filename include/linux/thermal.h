10ad4e2ee1d7bd68d2c7c0d50cb052920305f455
Daniel Lezcano <daniel.lezcano@oss.qualcomm.com>
2026-04-08 20:52:39 +0200

    thermal/core: Register cooling device non-OF drivers
    Provide a non-OF functions to register a cooling device in order to
    have a clear separation between OF and non-OF code. Drivers not using
    a devicetree can then migrate to this function.
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@oss.qualcomm.com>

--- include/linux/thermal.h
+++ include/linux/thermal.h
@@ -260,6 +260,11 @@ devm_thermal_of_cooling_device_register(struct device *dev,
 				struct device_node *np,
 				const char *type, void *devdata,
 				const struct thermal_cooling_device_ops *ops);
+
+struct thermal_cooling_device *
+devm_thermal_cooling_device_register(struct device *dev, const char *type,
+				     void *devdata, const struct thermal_cooling_device_ops *ops);
+
 void thermal_cooling_device_update(struct thermal_cooling_device *);
 void thermal_cooling_device_unregister(struct thermal_cooling_device *);
 struct thermal_zone_device *thermal_zone_get_zone_by_name(const char *name);
@@ -314,6 +319,14 @@ devm_thermal_of_cooling_device_register(struct device *dev,
 {
 	return ERR_PTR(-ENODEV);
 }
+
+struct thermal_cooling_device *
+devm_thermal_cooling_device_register(struct device *dev, const char *type,
+				     void *devdata, const struct thermal_cooling_device_ops *ops)
+{
+	return ERR_PTR(-ENODEV);
+}
+
 static inline void thermal_cooling_device_unregister(
 	struct thermal_cooling_device *cdev)
 { }
